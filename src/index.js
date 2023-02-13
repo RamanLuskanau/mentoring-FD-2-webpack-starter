0a0c08dd9997c38a73f415fafa160d7459d90e25
Raman Luskanau <r_o_m_a_lu@mail.ru>
2023-02-13 13:56:25 +0300

    drumPud

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import './main.scss';
 
-console.log('Hello IT-academy student 🚀');
+const pads = document.querySelectorAll('.pad');
+const visual = document.querySelector('.visual');
+
+export const KEY_MAP = {
+  kick: 'a',
+  cymbal: 's',
+  snare: 'd',
+  openHat: 'f',
+  longCrash: 'g',
+  hitHat: 'h',
+};
+
+export const SOUNDS = [
+  '../assets/sounds/kick.wav',
+  '../assets/sounds/cymbal.wav',
+  '../assets/sounds/snare.wav',
+  '../assets/sounds/openhat.wav',
+  '../assets/sounds/long-crash.wav',
+  '../assets/sounds/hihat.wav',
+];
+
+const playSound = (index) => {
+  const sound = SOUNDS[index];
+  const audio = new Audio(sound);
+  audio.load();
+  audio.play();
+};
+
+const onKeyPress = ({ key }) => {
+  switch (key) {
+    case 'a':
+      playSound(0);
+      break;
+    case 's':
+      playSound(1);
+      break;
+    case 'd':
+      playSound(2);
+      break;
+    case 'f':
+      playSound(3);
+      break;
+    case 'g':
+      playSound(4);
+      break;
+    case 'h':
+      playSound(5);
+      break;
+
+    default:
+      console.error('Error!!! Wrong a key');
+      break;
+  }
+};
+
+window.addEventListener('keypress', onKeyPress);
